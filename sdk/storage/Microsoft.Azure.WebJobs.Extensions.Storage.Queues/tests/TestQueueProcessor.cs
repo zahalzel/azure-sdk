baaac23098d23cdc6b6b29702e3a4c8ef4932279
Zach Halzel <zahalzel@microsoft.com>
2025-06-04 11:10:03 -0400

    Add queue listener fixes (restored cleanly with LF endings)

---
+++ sdk/storage/Microsoft.Azure.WebJobs.Extensions.Storage.Queues/tests/TestQueueProcessor.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Storage.Queues.Models;
+using Microsoft.Azure.WebJobs.Host.Queues;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Storage.Queues.Tests
+{
+    public class TestQueueProcessor : QueueProcessor
+    {
+        public bool DeleteCalled { get; private set; }
+        public CancellationToken CapturedDeleteToken { get; private set; }
+
+        public TestQueueProcessor(QueueProcessorOptions options)
+            : base(options)
+        {
+        }
+
+        protected override async Task DeleteMessageAsync(QueueMessage message)
+        {
+            DeleteCalled = true;
+            await base.DeleteMessageAsync(message);
+        }
+    }
+}
